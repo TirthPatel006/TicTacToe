07a7ba9f60293129af2bb25bace3236b9e855367
TirthPatel06 <justtalk2tirth@gmail.com>
2025-07-09 21:40:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,6 @@ def move():
 
     winner = check_winner(board)
     return jsonify({"board": board, "winner": winner})
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
